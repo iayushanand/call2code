4c2848b05ef91de49c4fd63b3731b2980d72e550
flickowoa <flickerdroid211@gmail.com>
2025-07-05 02:05:57 +0530

    theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-    import Navbar from '$lib/Navbar.svelte';
-	import '../app.css';
-	
+	import Navbar from "$lib/Navbar.svelte";
+	import "../app.css";
+
 	let { children } = $props();
 </script>
 
 {@render children()}
 
-
 <Navbar></Navbar>
+
+<button class="btn preset-filled-tertiary-500">sdfsdf</button>
