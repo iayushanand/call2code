6cbf72c4173e11aec91bce771da37de43f9de67e
flickowoa <flickerdroid211@gmail.com>
2025-07-05 02:30:55 +0530

    stuff

---
+++ src/lib/TempText.svelte
@@ -0,0 +1,7 @@
+<div>
+    <div
+        class="text-[25vh] font-bold text-center mb-4 text-tertiary-100 mask-r-from-tertiary-contrast-50-950"
+    >
+        27°
+    </div>
+</div>
--- src/routes/weather/+page.svelte
+++ src/routes/weather/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+    import TempText from "$lib/TempText.svelte";
+</script>
+
 <div
     class="min-h-screen bg-gradient-to-br to-tertiary-400 flex items-center justify-center p-4"
-></div>
+>
+    <TempText />
+</div>
