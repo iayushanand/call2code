492e13a43b6d14dee08cf6377e4ac51602f9c6fa
flickowoa <flickerdroid211@gmail.com>
2025-07-05 02:44:14 +0530

    clouuuud

--- src/lib/WeatherDetail.svelte
+++ src/lib/WeatherDetail.svelte
@@ -1,3 +1,12 @@
-<div class="flex">
-    <div>☁</div>
+<script lang="ts">
+    import Cloud from "@lucide/svelte/icons/cloud";
+</script>
+
+<div class="flex gap-6">
+    <div>
+        <Cloud
+            class="h-[25vh] w-[25vh] mask-l-from-tertiary-contrast-50-950 text-tertiary-100"
+        />
+    </div>
+    <div class="h-[15vh] w-1 bg-tertiary-100 my-auto opacity-35"></div>
 </div>
